92c9fcce51f1aba986a89660ebe12ed7a8f1b72d
Kayan Freitas <kayan6f@gmail.com>
2020-04-20 11:04:51 -0300

    Finalização desafio ReactJS

--- src/App.js
+++ src/App.js
@@ -1,29 +1,86 @@
-import React from "react";
-
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [techsstring, setTechsstring] = useState('');
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+      api.get('repositories').then(response => {
+        setRepositories(response.data);
+      })
+  }, []);
+
+  async function handleAddRepository(e) {
+    e.preventDefault();
+
+    const techs = techsstring.split(',');
+
+    const data = {
+        title,
+        url,
+        techs,
+    };
+    console.log(data);
+
+    try{
+      var repository = await api.post('repositories', data);
+      setRepositories([...repositories, repository.data]);
+       } 
+       catch(err){
+          alert('Falha no engano ao cadastrar repositório, tente novamente!');
+       }         
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try{      
+      await api.delete(`repositories/${id}`);
+      setRepositories(repositories.filter(repository => repository.id !== id))
+       } 
+       catch(err){
+          alert('Falha no engano ao remover repositório, tente novamente!');
+       }     
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => <li key={repository.id}>{repository.title}
+        <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+          </li>
+           )}
+         
       </ul>
+      
+      <form onSubmit={handleAddRepository}>
+      <input 
+        placeholder="Título do repositório" 
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        />
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+        <input 
+        placeholder="URL"
+        value={url}
+        onChange={e => setUrl(e.target.value)}
+         />
+
+        <input
+         placeholder="Techs"
+         value={techsstring}
+         onChange={e => setTechsstring(e.target.value)}
+          />
+
+          
+      <button className="button" type="submit">Adicionar</button>
+      </form>
+
     </div>
   );
 }
